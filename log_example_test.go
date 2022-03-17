8b9a765ce0c941ab6ee44c2dd8933da65eb0cbc4
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-17 23:05:41 +0900

    fix: example_test

--- log_example_test.go
+++ log_example_test.go
@@ -2,7 +2,6 @@ package dl_test
 
 import (
 	"os"
-	"testing"
 
 	"github.com/task4233/dl"
 )
@@ -20,42 +19,5 @@ func Example() {
 	// Output:
 	// num:  123
 	// name: dl
-	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:18
-}
-
-func ExampleTestFInfo(t *testing.T) {
-	type MyInt int
-	var myNum MyInt = 123
-
-	dl.FInfo(os.Stdout, myNum)
-
-	// Output:
-	// [DeLog] info: 123 (dl_test.MyInt) log_example_test.go:28
-}
-
-func ExampleTestInfo(t *testing.T) {
-	type MyInt int
-	var myNum MyInt = 123
-
-	dl.Info(myNum) // default output sends to stderr
-
-	// Output:
-}
-
-func ExampleTestPrintf(t *testing.T) {
-	type MyInt int
-	var myNum MyInt = 123
-
-	dl.Printf("%v", myNum) // default output sends to stderr
-
-	// Output:
-}
-
-func ExamplePrintln(t *testing.T) {
-	type MyInt int
-	var myNum MyInt = 123
-
-	dl.Println(myNum) // default output sends to stderr
-
-	// Output:
+	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:17
 }
