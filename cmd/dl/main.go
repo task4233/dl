60c3b4622cfbdd7a172a271d25f824523804c097
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-19 17:43:57 +0900

    feat: enhance-messages-for-command

--- cmd/dl/main.go
+++ cmd/dl/main.go
@@ -8,8 +8,10 @@ import (
 	"github.com/task4233/dl"
 )
 
+var version string
+
 func main() {
-	if err := dl.New().Run(context.Background(), os.Args[1:]); err != nil {
+	if err := dl.New().Run(context.Background(), version, os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: \n%v\n", err)
 		os.Exit(1)
 	}
