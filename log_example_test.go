739da6c001c7372b75298b9d71cbe77e433be778
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-19 16:31:25 +0900

    feat: enhance example_test

--- log_example_test.go
+++ log_example_test.go
@@ -13,11 +13,14 @@ func Example() {
 	var myNum MyInt = 123
 
 	dl.Fprintln(os.Stdout, "num: ", num)
+	dl.Println("num: ", num)
 	dl.Fprintf(os.Stdout, "name: %s\n", name)
+	dl.Printf("name: %s", name)
 	dl.FInfo(os.Stdout, myNum)
+	dl.Info(myNum)
 
 	// Output:
 	// num:  123
 	// name: dl
-	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:17
+	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:19
 }
