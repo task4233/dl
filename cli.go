f44f82ccfa48b95fce89c6ab88efe725daab33b8
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-19 15:11:33 +0900

    feat: add a function for removing Git pre-commit script

--- cli.go
+++ cli.go
@@ -1,6 +1,7 @@
 package dl
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -27,17 +28,17 @@ func (d *DeLog) Run(ctx context.Context, args []string) error {
 	if len(args) == 0 {
 		return errors.New("no argument")
 	}
+	if len(args) == 1 {
+		args = append(args, ".")
+	}
+
 	switch args[0] {
 	case "clean":
-		if len(args) == 1 {
-			args = append(args, ".")
-		}
 		return d.Clean(ctx, args[1])
 	case "init":
-		if len(args) == 1 {
-			args = append(args, ".")
-		}
 		return d.Init(ctx, args[1])
+	case "remove":
+		return d.Remove(ctx, args[1])
 	default:
 		return d.usage(args[0])
 	}
@@ -66,13 +67,61 @@ func (d *DeLog) Clean(ctx context.Context, baseDir string) error {
 	})
 }
 
-const precommitScript = `#!/bin/sh
+// Remove deletes `$ dl clean` command from pre-commit script
+func (d *DeLog) Remove(ctx context.Context, baseDir string) error {
+	path := filepath.Join(baseDir, ".git", "hooks", "pre-commit")
+	FInfo(os.Stderr, path)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return err
+	}
+
+	f, err := os.OpenFile(path, os.O_RDWR, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	buf := []byte{}
+	if _, err := f.Read(buf); err != nil {
+		return err
+	}
+
+	idx := bytes.Index(buf, []byte(precommitScript))
+	if idx < 0 {
+		return nil
+	}
+	if len(buf) == len(precommitScript) {
+		return os.Remove(path)
+	}
+
+	if _, err := f.Write(append(buf[:idx], buf[idx+len(precommitScript)+1:]...)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Although these values will be casted to []byte, they are declared as constants
+// because that is't happened frequently.
+const (
+	precommitScript = `#!/bin/sh
 dl clean .
 git add .
 `
+	cleanCmd = "dl clean"
+)
 
 // Init inserts dl command into git pre-commit hook
 func (d *DeLog) Init(ctx context.Context, baseDir string) error {
+	if err := d.addGitHookScript(ctx, baseDir); err != nil {
+		return err
+	}
+
+	// TODO(#17): add feature of commented out
+	return nil
+}
+
+func (d *DeLog) addGitHookScript(ctx context.Context, baseDir string) error {
 	path := filepath.Join(baseDir, ".git", "hooks")
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return err
@@ -80,11 +129,21 @@ func (d *DeLog) Init(ctx context.Context, baseDir string) error {
 
 	path = filepath.Join(path, "pre-commit")
 
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
+
+	// It checks if `$ dl clean` has been installed or not.
+	// If so, not inserting codes.
+	buf := []byte{}
+	if _, err := f.Read(buf); err != nil {
+		return err
+	}
+	if bytes.Contains(buf, []byte(cleanCmd)) {
+		return nil
+	}
 
 	if _, err := fmt.Fprintf(f, precommitScript); err != nil {
 		return err
