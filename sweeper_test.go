004b86e6d601eeacd211784792949968de73ed5b
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-20 17:22:38 +0900

    feat: add tests for restore

--- sweeper_test.go
+++ sweeper_test.go
@@ -140,3 +140,38 @@ func TestEvacuate(t *testing.T) {
 	}
 
 }
+
+func TestRestore(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		srcFilePath string
+		dstFilePath string
+		wantErr     bool
+	}{
+		"failed with unexisted src file path": {
+			srcFilePath: "unexisted/filepath",
+			wantErr:     true,
+		},
+		"failed with irregal dst file path": {
+			srcFilePath: "testdata/restore/test.go",
+			dstFilePath: "/root",
+			wantErr:     true,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			err := NewSweeper().Restore(context.Background(), tt.srcFilePath, tt.dstFilePath)
+			if err != nil {
+				if (err != nil) != tt.wantErr {
+					t.Fatalf("unexpected error: want=%v, got=%v", tt.wantErr, err)
+				}
+				return
+			}
+		})
+	}
+}
