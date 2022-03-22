e16fb6563f8fa25edbc94ee113b5d29f62bbe200
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-22 21:05:51 +0900

    feat: add lacked tests

--- log_example_test.go
+++ log_example_test.go
@@ -11,16 +11,24 @@ func Example() {
 	name := "dl"
 	type MyInt int
 	var myNum MyInt = 123
+	var names []string = []string{"alice", "bob", "Charlie"}
 
 	dl.Fprintln(os.Stdout, "num: ", num)
 	dl.Println("num: ", num)
+
 	dl.Fprintf(os.Stdout, "name: %s\n", name)
 	dl.Printf("name: %s", name)
+
 	dl.FInfo(os.Stdout, myNum)
 	dl.Info(myNum)
+
+	dl.FInfo(os.Stdout, names)
+	dl.Info(names)
 
 	// Output:
 	// num:  123
 	// name: dl
-	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:19
+	//[DeLog] info: 123 (dl_test.MyInt) log_example_test.go:22
+	//[DeLog] info: []string{"alice", "bob", "Charlie"} ([]string) log_example_test.go:25
+
 }
